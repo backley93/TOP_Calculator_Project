27261cba6cdbd5802b6c8875263d604cd45cb2e5
Backley93 <branden.ackley@gmail.com>
2022-08-04 12:59:10 -0700

    Created a function that calls the helper functions add, subtract, multiply or divide.

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const operate = (a, b, operator) => 
+    
+    if(operator === '+') {
+       const sum = add(a, b);
+       return sum;
+    } else if(operator === '-') {
+       const difference = subtract(a, b);
+       return difference;
+    } else if(operator === '*') {
+        const product = multiply(a, b);
+        return product;
+    } else if(operator === '/') {
+        const quotient = divide(a, b);
+        return quotient;
+    } ;
